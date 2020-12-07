8925d9c265e0963b641c9cfde1e5e4b52a9e5ed4
Aleksandr <alexander.p.17@yandex.ru>
2020-12-07 10:14:14 +0500

    Добавьте файлы проекта.

---
+++ Module3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string MyName = "Jane";
+		byte MyAge = 27;
+		bool MyPet = true;
+		double MyShoe = 37.5;
+
+		Console.WriteLine("My name is" + MyName);
+		Console.WriteLine("My age is" + MyAge);
+		Console.WriteLine("Do I have a pet? -" + MyPet);
+		Console.WriteLine("My shoe size is " + MyShoe);
+	}
+}
