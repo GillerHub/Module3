bd14226e7354314fd79101d40ee29ba2d580ca54
Aleksandr <alexander.p.17@yandex.ru>
2020-12-07 17:01:05 +0500

    Опросник реализован.

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -4,14 +4,22 @@ public class Program
 {
 	public static void Main()
 	{
-		string MyName = "Jane";
-		byte MyAge = 27;
-		bool MyPet = true;
-		double MyShoe = 37.5;
+		//string MyName = "Jane";
+		//byte MyAge = 27;
+		//bool MyPet = true;
+		//double MyShoe = 37.5;
 
-		Console.WriteLine("My name is" + MyName);
-		Console.WriteLine("My age is" + MyAge);
-		Console.WriteLine("Do I have a pet? -" + MyPet);
-		Console.WriteLine("My shoe size is " + MyShoe);
+		//Console.WriteLine("My name is" + MyName);
+		//Console.WriteLine("My age is" + MyAge);
+		//Console.WriteLine("Do I have a pet? -" + MyPet);
+		//Console.WriteLine("My shoe size is " + MyShoe);
+
+		Console.Write("Введите имя: ");
+		var name = Console.ReadLine();
+		Console.Write("Введите возраст: ");
+		var age = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Введите дату рождения: ");
+		var birthday = Console.ReadLine();
+		Console.WriteLine("Ваш день рождения {0}", birthday);
 	}
 }
